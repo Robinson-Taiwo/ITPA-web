60a93db923138efd7a21dd3bb2bddbfee8f2fc26
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2023-11-08 05:30:31 +0100

    contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import PageLayout from './PageLayout'
+import "./Contact.css"
 
 const Contact = () => {
   return (
-    <PageLayout>Contact</PageLayout>
+    <PageLayout>
+
+      <div className="contacts-page">
+
+
+
+        <div className="contact-header">
+
+          <h1>Contact Us</h1>
+        </div>
+
+      </div>
+
+
+      <div className="contacts-body">
+        
+      </div>
+
+
+
+
+    </PageLayout>
   )
 }
 
