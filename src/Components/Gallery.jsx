f54e189d9b425cf00f81b14321c6eac15b6a1658
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2023-11-09 18:15:26 +0100

    gallery page

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -14,8 +14,8 @@ const Gallery = () => {
 
             <div className="events-container">
                 <h2>ITPA'S Gallery</h2>
-                
-                <p></p>
+
+                <p>Embark on a visual exploration of the Information Technology Professionals Association (ITPA) Gallery. Immerse yourself in the dynamic world of technology through a curated collection of images capturing the essence of IT professionals at work.</p>
 
 
                 <div className="masonry-container">
